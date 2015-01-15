09704216ccb77f64ce674c61a4be05c665c3df2d
Yogthos <dmitri.sotnikov@gmail.com>
2015-01-14 20:47:26 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cryogen-asciidoc "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cryogen-core "0.1.16"]
+                 [org.asciidoctor/asciidoctorj "1.5.2"]])
---
+++ src/cryogen_asciidoc/core.clj
@@ -0,0 +1,25 @@
+(ns cryogen-asciidoc.core
+  (:require [cryogen-core.markup :refer [rewrite-hrefs markup-registry]]
+            [clojure.string :as s])
+  (:import org.asciidoctor.Asciidoctor$Factory
+           java.util.Collections
+           cryogen_core.markup.Markup))
+
+(defn asciidoc
+  "Returns an Asciidoc (http://asciidoc.org/) implementation of the
+  Markup protocol."
+  []
+  (reify Markup
+    (dir [this] "asc")
+    (ext [this] ".asc")
+    (render-fn [this]
+      (fn [rdr config]
+        (->>
+          (.convert (Asciidoctor$Factory/create)
+                   (->> (java.io.BufferedReader. rdr)
+                        (line-seq)
+                        (s/join "\n"))
+                   (Collections/emptyMap))
+          (rewrite-hrefs (:blog-prefix config)))))))
+
+(swap! markup-registry conj (asciidoc))
