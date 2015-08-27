39407f8bdad8767bd7a9e54303b977ecf3284f6e
atankanow <adam.tankanow@gmail.com>
2015-08-26 21:06:27 -0400

    Update version to 0.1.2-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-asciidoc "0.1.1"
+(defproject cryogen-asciidoc "0.1.2-SNAPSHOT"
   :description "AsciiDoc parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-asciidoc"
   :license {:name "Eclipse Public License"
