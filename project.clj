d6990c114da79919f62d5e22223e406eb8194055
Abhinav Sharma <abhi18av@users.noreply.github.com>
2017-11-09 10:01:38 +0530

    updated the asciidoctorj version

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cryogen-core "0.1.25"]
-                 [org.asciidoctor/asciidoctorj "1.5.2"]])
+                 [org.asciidoctor/asciidoctorj "1.5.6"]])
