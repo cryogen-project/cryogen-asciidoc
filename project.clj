bf83cae1ef6034919f0c85a24ab01b31f9e834f6
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-10-16 14:54:55 -0400

    bumped up cryogen-core

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cryogen-asciidoc "0.3.0"
+(defproject cryogen-asciidoc "0.3.1"
   :description "AsciiDoc parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-asciidoc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [cryogen-core "0.3.1"]
+                 [cryogen-core "0.3.2"]
                  [org.asciidoctor/asciidoctorj "2.4.1"]])
