5aef5df6d8f5223bec52a89331a8c12660253f7b
Jakub Holy <jakubholy@jakubholy.net>
2023-11-22 02:06:47 +0100

    Upgrade to AsciidoctorJ v3
    Upgrade to the latest AsciidoctorJ v3.0.
    
    Also, add deps.edn for easier inclusion in deps-based projects.
    
    Bump version of the plugin itself to 1 - it has been stable for a long time.
    
    Key code changes:
    
    * Use the OptionsBuilder -> Options (though as little as possible) instead of the
      deprecated map argument.
    * When cryogen's `:debug?` is on, print the final options - useful in troubleshooting

--- src/cryogen_asciidoc/core.clj
+++ src/cryogen_asciidoc/core.clj
@@ -48,27 +48,40 @@
   ([a b & more]
    (reduce deep-merge (or a {}) (cons b more))))
 
+(defn options-map->Options [{:keys [attributes] :as opts-map}]
+  (let [opts (-> (Options/builder)
+                (.build))]
+    (doseq [[k v] (dissoc opts-map :attributes)]
+      (.setOption opts k v))
+    (some->> attributes (.setAttributes opts))
+    opts))
+
+(defn asciidoc-opts ^Options [{:keys [asciidoctor debug? page-meta] :as config}]
+  (let [opts-map
+        (deep-merge
+         {Options/SAFE (.getLevel SafeMode/SAFE)}
+         (keys->strings asciidoctor)
+         (keys->strings (:asciidoctor page-meta)))]
+    (when debug?
+      (println "DEBUG cryogen-asciidoc: options =" opts-map))
+    (options-map->Options opts-map)))
+
 (defn asciidoc
   "Returns an Asciidoc (http://asciidoc.org/) implementation of the
   Markup protocol."
   []
   (reify Markup
-    (dir [this] "asc")
-    (exts [this] #{".adoc" ".ad" ".asciidoc" ".asc"})
-    (render-fn [this]
+    (dir [_this] "asc")
+    (exts [_this] #{".adoc" ".ad" ".asciidoc" ".asc"})
+    (render-fn [_this]
       (fn [rdr config]
         (->>
-          (.convert (get-or-init-adoc adoc (:asciidoctor config))
+         (.convert (get-or-init-adoc adoc (:asciidoctor config))
                    (->> (java.io.BufferedReader. rdr)
                         (line-seq)
                         (s/join "\n"))
-                   (deep-merge
-                     {Options/SAFE (.getLevel SafeMode/SAFE)}
-                     (keys->strings
-                       (:asciidoctor config))
-                     (keys->strings
-                       (-> config :page-meta :asciidoctor))))
-          (rewrite-hrefs (:blog-prefix config)))))))
+                   (asciidoc-opts config))
+         (rewrite-hrefs (:blog-prefix config)))))))
 
 (defn init []
   (swap! markup-registry conj (asciidoc)))
